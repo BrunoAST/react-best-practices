9727d1e8838c073e2090f8c3e8c3dbb73b560249
Bruno Alves <bruno-alves2010@live.com>
2021-08-09 16:28:29 -0300

    test: add generic params to HttpPostClient

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,14 +1,14 @@
 import {HttpPostClient, HttpPostParams} from 'data/protocols/http/http-post-client';
 import {HttpResponse, HttpStatusCode} from "../protocols/http/http-response";
 
-export class HttpPostClientSpy implements HttpPostClient {
+export class HttpPostClientSpy<BodyType, ResponseType> implements HttpPostClient<BodyType, ResponseType> {
     url?: string;
-    body?: object;
-    response: HttpResponse = {
+    body?: BodyType;
+    response: HttpResponse<ResponseType> = {
         statusCode: HttpStatusCode.ok
     };
 
-    post(params: HttpPostParams): Promise<HttpResponse> {
+    post(params: HttpPostParams<BodyType>): Promise<HttpResponse<ResponseType>> {
         this.url = params.url;
         this.body = params.body;
         return Promise.resolve(this.response);
