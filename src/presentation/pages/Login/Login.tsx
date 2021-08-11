4d1c623179aefedb7031c0fbd8e61f385ed583f7
Bruno Alves <bruno-alves2010@live.com>
2021-08-11 19:59:16 -0300

    feat: ensure Login will not render spinner and error on start

--- src/presentation/pages/Login/Login.tsx
+++ src/presentation/pages/Login/Login.tsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import Styles from "./login.module.scss";
 import LoginHeader from "../../components/LoginHeader/LoginHeader";
 import Footer from "../../components/Footer/Footer";
 import Input from "../../components/Input/Input";
 import FormStatus from "../../components/FormStatus/FormStatus";
+import Context from "../../context/form/form-context";
+
+type StateProps = {
+    isLoading: boolean;
+    errorMessage: string;
+}
 
 const Login: React.FC = () => {
+    const [state] = useState<StateProps>({isLoading: false, errorMessage: ""});
+
     return (
-        <div className={Styles.login}>
+        <div data-testid="" className={Styles.login}>
             <LoginHeader/>
-            <form className={Styles.login__form}>
-                <h2>Login</h2>
+            <Context.Provider value={state}>
+                <form className={Styles.login__form}>
+                    <h2>Login</h2>
 
-                <Input type="email" name="email" placeholder="Digite seu e-mail"/>
-                <Input type="password" name="password" placeholder="Digite sua senha"/>
+                    <Input type="email" name="email" placeholder="Digite seu e-mail"/>
+                    <Input type="password" name="password" placeholder="Digite sua senha"/>
 
-                <button className={Styles.submit} type="submit">Entrar</button>
-                <span className={Styles.link}>Criar conta</span>
+                    <button className={Styles.submit} type="submit">Entrar</button>
+                    <span className={Styles.link}>Criar conta</span>
 
-                <FormStatus/>
-            </form>
+                    <FormStatus/>
+                </form>
+            </Context.Provider>
             <Footer/>
         </div>
     );
