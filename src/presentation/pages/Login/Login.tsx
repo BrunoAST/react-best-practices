28fe0cdc23c1575ffab7be609cc1b00eb89543d1
Bruno Alves <bruno-alves2010@live.com>
2021-08-11 12:57:35 -0300

    refactor: move form status to a separate file

--- src/presentation/pages/Login/Login.tsx
+++ src/presentation/pages/Login/Login.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Styles from "./login-styles.scss";
-import Spinner from "../../components/Spinner/Spinner";
 import LoginHeader from "../../components/LoginHeader/LoginHeader";
 import Footer from "../../components/Footer/Footer";
 import Input from "../../components/Input/Input";
+import FormStatus from "../../components/FormStatus/FormStatus";
 
 const Login: React.FC = () => {
     return (
@@ -11,23 +11,15 @@ const Login: React.FC = () => {
             <LoginHeader/>
             <form className={Styles.login__form}>
                 <h2>Login</h2>
-                {/*Email*/}
-                <Input type="email" name="email" placeholder="Digite seu e-mail"/>
 
-                {/*Password*/}
+                <Input type="email" name="email" placeholder="Digite seu e-mail"/>
                 <Input type="password" name="password" placeholder="Digite sua senha"/>
 
-                {/*Actions*/}
                 <button className={Styles.submit} type="submit">Entrar</button>
                 <span className={Styles.link}>Criar conta</span>
 
-                {/*Error messages*/}
-                <div className={Styles.errorWrap}>
-                    <Spinner className={Styles.spinner}/>
-                    <span className={Styles.error}>Erro</span>
-                </div>
+                <FormStatus/>
             </form>
-
             <Footer/>
         </div>
     );
