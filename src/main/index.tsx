82f91e79e742aec803faabd26f36a80e174c00ce
Bruno Alves <bruno-alves2010@live.com>
2021-08-11 10:56:36 -0300

    feat: add login view

---
+++ src/main/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Login from "../presentation/pages/Login/Login";
+
+ReactDOM.render(
+    <Login/>,
+    document.getElementById("main")
+)
