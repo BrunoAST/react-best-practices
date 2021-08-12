1918182d14d7f982dc570442ec123c11ab28ecc5
Bruno Alves <bruno-alves2010@live.com>
2021-08-12 13:33:33 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/Login/Login.spec.tsx
+++ src/presentation/pages/Login/Login.spec.tsx
@@ -1,30 +1,35 @@
 import React from "react";
-import {render, screen} from "@testing-library/react";
+import {render, RenderResult} from "@testing-library/react";
 import Login from "./Login";
 
-beforeEach(() => {
-    render(<Login/>);
-})
+type SutTypes = {
+    sut: RenderResult;
+}
+
+const makeSut = (): SutTypes => {
+    const sut = render(<Login/>);
+    return {sut};
+}
 
 describe("Login Component", () => {
     it("Should not render spinner and error on start", () => {
-        const errorWrap = screen.getByTestId("error-wrap");
+        const errorWrap = makeSut().sut.getByTestId("error-wrap");
         expect(errorWrap.childElementCount).toBe(0);
     });
 
     it("Should start with submit button disabled", () => {
-        const submitButton = screen.getByTestId("submit-button") as HTMLButtonElement;
+        const submitButton = makeSut().sut.getByTestId("submit-button") as HTMLButtonElement;
         expect(submitButton.disabled).toBeTruthy();
     });
 
     it("Should start with the initial status label for email input", () => {
-        const emailStatus = screen.getByTestId("email-status");
+        const emailStatus = makeSut().sut.getByTestId("email-status");
         expect(emailStatus.title).toBe("Campo obrigatório");
         expect(emailStatus.textContent).toBe("🔴");
     });
 
     it("Should start with the initial status label for password input", () => {
-        const passwordStatus = screen.getByTestId("password-status");
+        const passwordStatus = makeSut().sut.getByTestId("password-status");
         expect(passwordStatus.title).toBe("Campo obrigatório");
         expect(passwordStatus.textContent).toBe("🔴");
     });
