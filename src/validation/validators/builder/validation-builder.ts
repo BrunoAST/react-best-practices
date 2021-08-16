1e89c50ff9d8366b593b04e174da8c04749cde82
Bruno Alves <bruno-alves2010@live.com>
2021-08-16 19:00:13 -0300

    refactor: ensure ValidationBuilder has a email helper

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,5 +1,6 @@
 import {FieldValidation} from "../../protocols/field-validation";
 import {RequiredFieldValidation} from "../required-field/required-field-validation";
+import {EmailValidation} from "../email/email-validation";
 
 export class ValidationBuilder {
     private constructor(
@@ -15,6 +16,11 @@ export class ValidationBuilder {
 
     required(): ValidationBuilder {
         this.validations.push(new RequiredFieldValidation(this.fieldName));
+        return this;
+    }
+
+    email(): ValidationBuilder {
+        this.validations.push(new EmailValidation(this.fieldName));
         return this;
     }
 
