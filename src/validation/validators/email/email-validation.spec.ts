40b24c251899ccfe74146829c12f153cf2afb539
Bruno Alves <bruno-alves2010@live.com>
2021-08-16 11:47:57 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
+import faker from "faker";
 import {EmailValidation} from "./email-validation";
 import {InvalidEmailError} from "../../errors/invalid-email-error";
 
 describe("EmailValidation", () => {
     it("Should return error if email is invalid", () => {
-        const sut = new EmailValidation("email");
-        const error = sut.validate("");
+        const sut = new EmailValidation(faker.database.column());
+        const error = sut.validate(faker.random.word());
         expect(error).toEqual(new InvalidEmailError());
+    });
+
+    it("Should return falsy if email is valid", () => {
+        const sut = new EmailValidation(faker.database.column());
+        const error = sut.validate(faker.internet.email());
+        expect(error).toBeFalsy();
     });
 });
