43a99bd677c99976c04eb16c80db407ba75fea8f
Bruno Alves <bruno-alves2010@live.com>
2021-08-18 19:07:17 -0300

    feat: ensure LocalSaveAccessToken calls SetStorage with correct value

---
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -0,0 +1,12 @@
+import {SaveAccessToken} from "../../../domain/usecases/save-access-token";
+import {SetStorage} from "../../protocols/cache/set-storage";
+
+export class LocalSaveAccessToken implements SaveAccessToken {
+    constructor(private readonly setStorage: SetStorage) {
+    }
+
+    async save(accessToken: string): Promise<void> {
+        await this.setStorage.set("accessToken", accessToken);
+        return Promise.resolve();
+    }
+}
