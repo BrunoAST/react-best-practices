98981d109adda9f5910ab7a29d4f1d8b195de9eb
Bruno Alves <bruno-alves2010@live.com>
2021-08-23 13:10:58 -0300

    feat: ensure SignUp change fields status to error if validation fails

--- src/presentation/pages/SignUp/SignUp.tsx
+++ src/presentation/pages/SignUp/SignUp.tsx
@@ -13,13 +13,13 @@ const SignUp: React.FC<Props> = ({validation}: Props) => {
     const [state, setState] = useState({
         isLoading: false,
         name: "",
-        // email: "",
-        // password: "",
-        // passwordConfirmation: "",
-        nameError: "Campo obrigatório",
-        emailError: "Campo obrigatório",
-        passwordError: "Campo obrigatório",
-        passwordConfirmationError: "Campo obrigatório",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+        nameError: "",
+        emailError: "",
+        passwordError: "",
+        passwordConfirmationError: "",
         mainError: "",
     });
 
@@ -27,8 +27,11 @@ const SignUp: React.FC<Props> = ({validation}: Props) => {
         setState({
             ...state,
             nameError: validation.validate("name", state.name),
+            emailError: validation.validate("email", state.email),
+            passwordError: validation.validate("password", state.password),
+            passwordConfirmationError: validation.validate("passwordConfirmation", state.passwordConfirmation)
         });
-    }, [state.name]);
+    }, [state.name, state.email, state.password, state.passwordConfirmation]);
 
     return (
         <div className={Styles.formWrapper}>
