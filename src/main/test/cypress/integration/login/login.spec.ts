82913546ed0468cbd16f90a5a74ea869a18a920a
Bruno Alves <bruno-alves2010@live.com>
2021-08-26 11:45:22 -0300

    test: ensure Login begin with initial state

--- src/main/test/cypress/integration/login/login.spec.ts
+++ src/main/test/cypress/integration/login/login.spec.ts
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
+beforeEach(() => {
+    cy.visit("login");
+});
+
 describe("Login", () => {
     it("Should load with correct initial state", () => {
-       cy.visit("login");
+        cy.getByTestId("email-status")
+            .should("have.attr", "title", "Campo obrigatório")
+            .should("contain.text", "🔴");
+        cy.getByTestId("password-status")
+            .should("have.attr", "title", "Campo obrigatório")
+            .should("contain.text", "🔴");
+        cy.getByTestId("submit-button").should("be.disabled");
+        cy.getByTestId("error-wrap").should("not.have.descendants");
     });
 });
