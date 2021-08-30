425a4ca581cbbc4a329224099d5e4f51bb13c3ba
Bruno Alves <bruno-alves2010@live.com>
2021-08-30 09:54:20 -0300

    feat: ensure LocalSaveAccessToken throws if no accessToken is provided

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -1,12 +1,15 @@
 import {SaveAccessToken} from "../../../domain/usecases/save-access-token";
 import {SetStorage} from "../../protocols/cache/set-storage";
+import {UnexpectedError} from "../../../domain/errors/unexpected-error";
 
 export class LocalSaveAccessToken implements SaveAccessToken {
     constructor(private readonly setStorage: SetStorage) {
     }
 
     async save(accessToken: string): Promise<void> {
+        if (!accessToken) {
+            throw new UnexpectedError();
+        }
         await this.setStorage.set("accessToken", accessToken);
-        return Promise.resolve();
     }
 }
