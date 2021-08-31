d646685c6a5c9aacea69323a498dc2c8d86980df
Bruno Alves <bruno-alves2010@live.com>
2021-08-31 09:11:12 -0300

    refactor: move Login mocks to support folder

--- src/main/test/cypress/integration/login/login.spec.ts
+++ src/main/test/cypress/integration/login/login.spec.ts
@@ -5,7 +5,7 @@ import {isWrapInvalid} from "../../support/assertions/input";
 import * as SubmitButtonAssertions from "../../support/assertions/submit-button";
 import * as FormStatusAssertions from "../../support/assertions/form-status";
 import {urlEquals} from "../../support/assertions/url";
-import {mockInvalidCredentialsError, mockInvalidData, mockOk, mockUnexpectedError} from "./login-mocks";
+import {mockInvalidCredentialsError, mockInvalidData, mockOk, mockUnexpectedError} from "../../support/mocks/login-mocks";
 import {testLocalStorageItem} from "../../support/assertions/local-storage";
 import {clickSubmitButton} from "../../support/assertions/submit-button";
 
