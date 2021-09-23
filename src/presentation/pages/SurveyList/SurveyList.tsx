f4a349d9e77a8e8e73214fae34311436b205b445
Bruno Alves <bruno-alves2010@live.com>
2021-09-23 19:03:36 -0300

    feat: create theme file for icons

--- src/presentation/pages/SurveyList/SurveyList.tsx
+++ src/presentation/pages/SurveyList/SurveyList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "../../components/Logo/Logo";
 import Footer from "../../components/Footer/Footer";
 import Styles from "./survey-list.module.scss";
+import { thumbDownIcon } from "../../../theme/assets/icons";
 
 const SurveyList: React.FC = () => {
 	return (
@@ -21,7 +22,7 @@ const SurveyList: React.FC = () => {
 					<li>
 						<div className={Styles.surveyContent}>
 							<div className={`${Styles.surveyContent__iconWrap} ${Styles.red}`}>
-								<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII=" alt="Thumb down" />
+								<img src={thumbDownIcon} alt="Thumb down" />
 							</div>
 							<time>
 								<span className={Styles.day}>20</span>
