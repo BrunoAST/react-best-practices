ea51f688fac744c0aa85288ae17be529cd3a05ee
Bruno Alves <bruno-alves2010@live.com>
2022-03-15 21:01:36 -0300

    style: create styles for load survey list error

--- src/presentation/pages/SurveyList/components/SurveyError/SurveyError.tsx
+++ src/presentation/pages/SurveyList/components/SurveyError/SurveyError.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import SurveyListContext from "../../context/survey-list-context";
+import Styles from "./survey-error.module.scss";
 
 const SurveyError: React.FC = () => {
   const { state, setState } = useContext(SurveyListContext);
@@ -9,7 +10,7 @@ const SurveyError: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className={Styles.errorWrap}>
       <span data-testid="error">{state.error}</span>
       <button data-testid="reload" onClick={reload}>Recarregar</button>
     </div>
