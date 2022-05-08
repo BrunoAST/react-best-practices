030efdccd8f487e1c65ca593309adbca472afb31
Bruno Alves <bruno-alves2010@live.com>
2022-05-08 18:53:29 -0300

    test: ensure AuthorizeHttpGetClientDecorator merge token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -35,7 +35,7 @@ describe("AuthorizeHttpGetClientDecorator", () => {
     expect(httpGetClientSpy.headers).toEqual(httpRequest.headers);
   });
 
-    test("Should add headers to HttpGetClient", async () => {
+  test("Should add headers to HttpGetClient", async () => {
     const { sut, getStorageSpy, httpGetClientSpy } = makeSut();
     getStorageSpy.value = mockAccountModel();
     const httpRequest: HttpGetParams = {
@@ -44,6 +44,22 @@ describe("AuthorizeHttpGetClientDecorator", () => {
     await sut.get(httpRequest);
     expect(httpGetClientSpy.url).toBe(httpRequest.url);
     expect(httpGetClientSpy.headers).toEqual({
+      "x-access-token": getStorageSpy.value.accessToken
+    });
+  });
+
+  test("Should merge headers to HttpGetClient", async () => {
+    const { sut, getStorageSpy, httpGetClientSpy } = makeSut();
+    getStorageSpy.value = mockAccountModel();
+    const field = faker.random.word();
+    const httpRequest: HttpGetParams = {
+      url: faker.internet.url(),
+      headers: { field }
+    };
+    await sut.get(httpRequest);
+    expect(httpGetClientSpy.url).toBe(httpRequest.url);
+    expect(httpGetClientSpy.headers).toEqual({
+      field,
       "x-access-token": getStorageSpy.value.accessToken
     });
   });
