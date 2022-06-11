d76980d1245145e30741445fd85b63cd35e375b5
Bruno Alves <bruno-alves2010@live.com>
2022-06-11 17:20:42 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,11 +1,7 @@
 import { AccountModel } from "../../domain/models/account-model";
-import { UnexpectedError } from "../../domain/errors/unexpected-error";
 import { makeLocalStorageAdapter } from "../factories/cache/local-storage-adapter-factory";
 
 export const setCurrentAccountAdapter = (accountModel: AccountModel): void => {
-  if (!accountModel?.accessToken) {
-    throw new UnexpectedError();
-  }
   makeLocalStorageAdapter().set("account", accountModel);
 }
 
--- src/main/adapters/test/current-account-adapter.spec.ts
+++ src/main/adapters/test/current-account-adapter.spec.ts
@@ -1,7 +1,6 @@
 import { setCurrentAccountAdapter, getCurrentAccountAdapter } from "../current-account-adapter";
 import { mockAccountModel } from "../../../domain/test/mock-account";
 import { LocalStorageAdapter } from "../../../infra/cache/local-storage-adapter";
-import { UnexpectedError } from "../../../domain/errors/unexpected-error";
 
 jest.mock("../../../infra/cache/local-storage-adapter");
 
@@ -19,11 +18,5 @@ describe("CurrentAccountAdapter", () => {
     const result = getCurrentAccountAdapter();
     expect(getSpy).toHaveBeenCalledWith("account")
     expect(result).toEqual(account);
-  });
-
-  test("Should throw UnexpectedError", () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined);
-    }).toThrow(new UnexpectedError());
   });
 });
